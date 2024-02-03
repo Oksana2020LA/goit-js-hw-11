bdd0c55ffef52b0631238120125a20c19106b9ba
Oksana2020LA <oksana2020la@gmail.com>
2024-02-02 23:59:26 -0800

    update

--- src/main.js
+++ src/main.js
@@ -48,7 +48,7 @@ function searchImages(searchTerm) {
       if (data.hits.length === 0) {
         iziToast.error({
           message: 'Sorry, there are no images matching your search query.<br>Please try again!</br>',
-          position: 'topRight',
+          position: 'center',
           transitionIn: "fadeInLeft",
         });
         hideLoader()
